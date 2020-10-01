d7193f723f6021bb410407ff88251ac3ecbb87c3
Thomas Linse <tjl4709@rit.edu>
2020-10-01 15:16:50 -0400

    added constructor to Report

--- LBMS/src/DataClasses/Report.java
+++ LBMS/src/DataClasses/Report.java
@@ -7,4 +7,13 @@ public class Report
     private int numVisitors, numBooksOwned;
     private double avgVisitTimeInHours, totalBookFines;
     private List<Book> booksPurchased;
+
+    public Report(int visitors, int booksOwned, double avgTime, List<Book> booksBought, double totalFines)
+    {
+        numVisitors = visitors;
+        numBooksOwned = booksOwned;
+        avgVisitTimeInHours = avgTime;
+        booksPurchased = booksBought;
+        totalBookFines = totalFines;
+    }
 }
