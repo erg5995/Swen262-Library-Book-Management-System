c38cabe2b9601a17a2959c133dd6d26f09a7ff96
Thomas Linse <tjl4709@rit.edu>
2020-10-01 14:09:29 -0400

    added global variables to Transaction

--- LBMS/src/DataClasses/Transaction.java
+++ LBMS/src/DataClasses/Transaction.java
@@ -1,9 +1,16 @@
 package DataClasses;
 
+import java.time.LocalDate;
+
 /**
  * Dataclass for transactions
  * Author: Thomas Linse
  */
 public class Transaction
 {
+    private Book book;
+    private User user;
+    private LocalDate dueDate, dateChecked;
+    private double fine;
+    private boolean isOverdue;
 }
