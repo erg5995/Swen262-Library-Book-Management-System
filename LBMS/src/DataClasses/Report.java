2e82e0fa53b3f16b56717786e0e9b00a8d5a794d
Thomas Linse <tjl4709@rit.edu>
2020-10-01 15:13:26 -0400

    created Report and added global variables

---
+++ LBMS/src/DataClasses/Report.java
@@ -0,0 +1,10 @@
+package DataClasses;
+
+import java.util.List;
+
+public class Report
+{
+    private int numVisitors, numBooksOwned;
+    private double avgVisitTimeInHours, totalBookFines;
+    private List<Book> booksPurchased;
+}
