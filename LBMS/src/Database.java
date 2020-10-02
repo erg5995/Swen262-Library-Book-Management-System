1fd7cca039a928de8f0903029c3e18f3b1fa6248
Thomas Linse <tjl4709@rit.edu>
2020-10-02 12:30:11 -0400

    added global variables to Database

--- LBMS/src/Database.java
+++ LBMS/src/Database.java
@@ -1,7 +1,18 @@
+import DataClasses.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Database class that stores all required info
  * Author: Thomas Linse
  */
 public class Database
 {
+    private Map<String, Book> booksOwned, booksInStore;
+    private List<Book> librarySearch, storeSearch;
+    private List<Transaction> checkedOutBooks, returnedBooks;
+    private List<Visit> visits;
+    private Map<Integer, User> users;
 }
