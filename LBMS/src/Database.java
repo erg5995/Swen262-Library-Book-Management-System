c35d539af3eac4ca03a41052be2e6bfc0fb41ba8
Thomas Linse <tjl4709@rit.edu>
2020-10-02 15:35:30 -0400

    implemented methods to find books matching query

--- LBMS/src/Database.java
+++ LBMS/src/Database.java
@@ -1,9 +1,6 @@
 import DataClasses.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Database class that stores all required info
@@ -26,5 +23,52 @@ public class Database
             if (user.isSame(fname, lname, address, phone))
                 return true;
         return false;
+    }
+    public void addUser(User user) { users.put(user.getId(), user); }
+    public User getUser(int id) { return users.get(id); }
+
+    public void addVisit(Visit visit) { visits.add(visit); }
+
+    public List<Book> bookInfoSearch(Book param, boolean forLibrary)
+    {
+        if (forLibrary) {
+            searchBooks(param, booksOwned.values(), librarySearch);
+            return librarySearch;
+        }
+        searchBooks(param, booksInStore.values(), storeSearch);
+        return storeSearch;
+    }
+    private void searchBooks(Book param, Collection<Book> search, List<Book> found)
+    {
+        found.clear();
+        for (Book book : search) {
+            if (!param.getTitle().equals("*") && !param.getTitle().equals(book.getTitle()))
+                continue;
+            if (!checkAuth(param.getAuthor(), book.getAuthor()))
+                continue;
+            if (param.getIsbn() != null && !param.getIsbn().equals("*") && !param.getIsbn().equals(book.getIsbn()))
+                continue;
+            if (param.getPublisher() != null && !param.getPublisher().equals("*") && !param.getPublisher().equals(book.getPublisher()))
+                continue;
+            found.add(book);
+        }
+    }
+    private boolean checkAuth(String[] pAuth, String[] bAuth)
+    {
+        if (pAuth == null)
+            return true;
+        boolean hasAuth;
+        for (String auth : pAuth)
+            if (!auth.equals("*")) {
+                hasAuth = false;
+                for (String check : bAuth)
+                    if (auth.equals(check)) {
+                        hasAuth = true;
+                        break;
+                    }
+                if (!hasAuth)
+                    return false;
+            }
+        return true;
     }
 }
