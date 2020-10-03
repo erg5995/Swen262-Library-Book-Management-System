34f560dddfff689783490ec9443af05649862daf
straviaz <joy.sangsrichan@gmail.com>
2020-10-02 22:57:33 -0400

    Stub methods

---
+++ LBMS/src/Calendar.java
@@ -0,0 +1,26 @@
+import java.time.LocalDateTime;
+
+public class Calendar {
+    LocalDateTime dateTime;
+//    Manager manager;
+
+    public void advance(LocalDateTime localDateTime) {
+
+    }
+
+    public LocalDateTime record(LocalDateTime localDateTime) {
+        return null;
+    }
+
+    public void open() {
+
+    }
+
+    public void close() {
+
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+}
