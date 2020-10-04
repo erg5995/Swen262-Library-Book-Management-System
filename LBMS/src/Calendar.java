af6ee8e20e2603aba49c2eb9d1968e5f16e0fb6a
straviaz <joy.sangsrichan@gmail.com>
2020-10-04 12:28:34 -0400

    Create a method to schedule tasks

--- LBMS/src/Calendar.java
+++ LBMS/src/Calendar.java
@@ -9,75 +9,65 @@ import java.util.TimerTask;
 class OpenLibraryTimeTask extends TimerTask {
 
     public void run() {
-
+        System.out.println("Open");
     }
+
 }
 
 class CloseLibraryTimeTask extends TimerTask {
 
     public void run() {
-
+        System.out.println("Close");
     }
 }
 
 public class Calendar {
-    private LocalDateTime dateTime;
+    private LocalDateTime currentTime;
     private Timer timer;
     private final LocalDateTime openingTime;
     private final LocalDateTime closingTime;
     private final long MILLISECOND_DAY = 86400 * 1000;
-    private boolean state = false;
 
     public Calendar() {
-        dateTime = LocalDateTime.now();
+        currentTime = LocalDateTime.now();
         timer = new Timer();
         // the date is just to start the recurring timer
         openingTime = LocalDateTime.of(2020, Month.OCTOBER, 4, 8, 0, 0);
-        closingTime = LocalDateTime.of(2020, Month.OCTOBER, 4, 11, 51, 0);
-        // start scheduled tasks
-        open();
-        close();
+        closingTime = LocalDateTime.of(2020, Month.OCTOBER, 4, 19, 0, 0);
+
+        startScheduledTasks();
     }
 
-    public boolean getState() {
-        return state;
-    }
-
-    public void setState(boolean newState) {
-        state = newState;
+    public void startScheduledTasks() {
+        // check if the hour is within closing time
+        if (currentTime.getHour() >= closingTime.getHour() || currentTime.getHour() < openingTime.getHour()) {
+            open();
+            close();
+        } else {
+            close();
+            open();
+        }
     }
 
     public void advanceDay(long day) {
-        dateTime = dateTime.plusDays(day);
+        currentTime = currentTime.plusDays(day);
     }
 
     public void advanceHour(long hour) {
-        dateTime = dateTime.plusHours(hour);
+        currentTime = currentTime.plusHours(hour);
     }
 
     public void open() {
-            timer.schedule(new OpenLibraryTimeTask(), Date.from(Instant.from(openingTime.atZone(ZoneId.systemDefault()))),
-                    MILLISECOND_DAY);
+        timer.schedule(new OpenLibraryTimeTask(), Date.from(Instant.from(openingTime.atZone(ZoneId.systemDefault()))),
+                MILLISECOND_DAY);
     }
 
     public void close() {
-            timer.schedule(new CloseLibraryTimeTask(), Date.from(Instant.from(closingTime.atZone(ZoneId.systemDefault()))),
-                    MILLISECOND_DAY);
+        timer.schedule(new CloseLibraryTimeTask(), Date.from(Instant.from(closingTime.atZone(ZoneId.systemDefault()))),
+                MILLISECOND_DAY);
     }
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public LocalDateTime getOpeningTime() {
-        return openingTime;
-    }
-
-    public LocalDateTime getClosingTime() {
-        return closingTime;
+    public LocalDateTime getCurrentTime() {
+        return currentTime;
     }
 }
--- LBMS/src/Main.java
+++ LBMS/src/Main.java
@@ -1,8 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        Calendar calendar = new Calendar();
-        System.out.println(calendar.getDateTime());
-        calendar.advanceDay(3);
-        System.out.println(calendar.getDateTime());
     }
 }
