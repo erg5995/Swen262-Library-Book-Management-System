a34608dc19198e395abc0a815f195edf3dac2959
straviaz <joy.sangsrichan@gmail.com>
2020-10-04 10:42:57 -0400

    Create scheduled tasks

--- LBMS/src/Calendar.java
+++ LBMS/src/Calendar.java
@@ -1,11 +1,29 @@
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+class LibraryStateTimeTask extends TimerTask {
+//    private Manger manger;
+
+    public void run() {
+
+    }
+}
 
 public class Calendar {
     private LocalDateTime dateTime;
-//    private Manager manager;
+    private Timer timer;
+    private final LocalDateTime openingTime;
+    private final LocalDateTime closingTime;
 
     public Calendar() {
         dateTime = LocalDateTime.now();
+        timer = new Timer();
+        openingTime = LocalDateTime.now();
+        closingTime = LocalDateTime.now();
     }
 
     public void advanceDay(long day) {
@@ -17,17 +35,17 @@ public class Calendar {
     }
 
     public LocalDateTime record(LocalDateTime localDateTime) {
-
-
         return null;
     }
 
     public void open() {
-
+        timer.schedule(new LibraryStateTimeTask(), Date.from(Instant.from(openingTime.atZone(ZoneId.systemDefault()))),
+                86400); // there are 86400 seconds in a day
     }
 
     public void close() {
-
+        timer.schedule(new LibraryStateTimeTask(), Date.from(Instant.from(closingTime.atZone(ZoneId.systemDefault()))),
+                86400); // there are 86400 seconds in a day
     }
 
     public LocalDateTime getDateTime() {
