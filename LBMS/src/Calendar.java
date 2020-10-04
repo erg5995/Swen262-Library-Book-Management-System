c002c0ef7ce984bd01c3f9ca1d43cdae144594e7
straviaz <joy.sangsrichan@gmail.com>
2020-10-04 11:55:14 -0400

    Separate opening and closing task

--- LBMS/src/Calendar.java
+++ LBMS/src/Calendar.java
@@ -1,12 +1,19 @@
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
-class LibraryStateTimeTask extends TimerTask {
-//    private Manger manger;
+class OpenLibraryTimeTask extends TimerTask {
+
+    public void run() {
+
+    }
+}
+
+class CloseLibraryTimeTask extends TimerTask {
 
     public void run() {
 
@@ -18,12 +25,26 @@ public class Calendar {
     private Timer timer;
     private final LocalDateTime openingTime;
     private final LocalDateTime closingTime;
+    private final long MILLISECOND_DAY = 86400 * 1000;
+    private boolean state = false;
 
     public Calendar() {
         dateTime = LocalDateTime.now();
         timer = new Timer();
-        openingTime = LocalDateTime.now();
-        closingTime = LocalDateTime.now();
+        // the date is just to start the recurring timer
+        openingTime = LocalDateTime.of(2020, Month.OCTOBER, 4, 8, 0, 0);
+        closingTime = LocalDateTime.of(2020, Month.OCTOBER, 4, 11, 51, 0);
+        // start scheduled tasks
+        open();
+        close();
+    }
+
+    public boolean getState() {
+        return state;
+    }
+
+    public void setState(boolean newState) {
+        state = newState;
     }
 
     public void advanceDay(long day) {
@@ -34,21 +55,29 @@ public class Calendar {
         dateTime = dateTime.plusHours(hour);
     }
 
-    public LocalDateTime record(LocalDateTime localDateTime) {
-        return null;
-    }
-
     public void open() {
-        timer.schedule(new LibraryStateTimeTask(), Date.from(Instant.from(openingTime.atZone(ZoneId.systemDefault()))),
-                86400); // there are 86400 seconds in a day
+            timer.schedule(new OpenLibraryTimeTask(), Date.from(Instant.from(openingTime.atZone(ZoneId.systemDefault()))),
+                    MILLISECOND_DAY);
     }
 
     public void close() {
-        timer.schedule(new LibraryStateTimeTask(), Date.from(Instant.from(closingTime.atZone(ZoneId.systemDefault()))),
-                86400); // there are 86400 seconds in a day
+            timer.schedule(new CloseLibraryTimeTask(), Date.from(Instant.from(closingTime.atZone(ZoneId.systemDefault()))),
+                    MILLISECOND_DAY);
     }
 
     public LocalDateTime getDateTime() {
         return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public LocalDateTime getOpeningTime() {
+        return openingTime;
+    }
+
+    public LocalDateTime getClosingTime() {
+        return closingTime;
     }
 }
