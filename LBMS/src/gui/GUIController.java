e785feb93e146373efde7ab41c0ba90aa64f9e94
Thomas Linse <tjl4709@rit.edu>
2020-10-29 17:19:59 -0400

    stubbed out GUIController

---
+++ LBMS/src/gui/GUIController.java
@@ -0,0 +1,44 @@
+package gui;
+
+import system.Client;
+
+import javafx.event.ActionEvent;
+
+public class GUIController
+{
+    Client client;
+
+    public GUIController(Client c) { client = c; }
+
+    /** Methods for user input */
+    public void searchPressed(ActionEvent e)
+    {}
+    public void buyPressed(ActionEvent e)
+    {}
+    public void arrivePressed(ActionEvent e)
+    {}
+    public void departPressed(ActionEvent e)
+    {}
+    public void borrowedPressed(ActionEvent e)
+    {}
+    public void returnPressed(ActionEvent e)
+    {}
+    public void infoPressed(ActionEvent e)
+    {}
+    public void borrowPressed(ActionEvent e)
+    {}
+    public void advancePressed(ActionEvent e)
+    {}
+    public void datetimePressed(ActionEvent e)
+    {}
+    public void payPressed(ActionEvent e)
+    {}
+    public void reportPressed(ActionEvent e)
+    {}
+    public void registerPressed(ActionEvent e)
+    {}
+    public void clearPressed(ActionEvent e)
+    {}
+    public void submitPressed(ActionEvent e)
+    {}
+}
